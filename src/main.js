d319d78dec27cc83e9df854b7424f140aeb0b16b
Kinga <kingaborycka@wp.pl>
2022-04-20 19:42:17 +0200

    vue.js base project

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+import router from './router'
 
 
-// Tworzona jest instancja Vue.
-// Renderowanie komponentu najwyższego poziomu przeniesione zostaje do App.vue.
-// Instancja Vue zostaje powiązana z elementem #app znajdującym się w pliku index.html.
+createApp(App).use(router).mount('#app')
+
+
